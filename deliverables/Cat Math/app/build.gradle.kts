035e4260f5434d3eb5703b18225225d2dc79fc52
benXhawk <22benhawk2004@gmail.com>
2024-11-17 17:07:32 -0500

    mathdrills layout and number entry

--- deliverables/Cat Math/app/build.gradle.kts
+++ deliverables/Cat Math/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
